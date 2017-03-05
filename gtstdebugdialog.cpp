753f494ee0fb4e5cc28c03d8a61144319efee6e7
richelbilderbeek <richel@richelbilderbeek.nl>
2017-03-05 10:54:00 +0100

    Improved

--- gtstdebugdialog.cpp
+++ gtstdebugdialog.cpp
@@ -1,23 +1,7 @@
-//---------------------------------------------------------------------------
-/*
-GTST, Game Theory Server
-Copyright (C) 2011-2015 Richel Bilderbeek
+#include "gtstdebugdialog.h"
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-//---------------------------------------------------------------------------
-//From http://www.richelbilderbeek.nl/ProjectGtst.htm
-//---------------------------------------------------------------------------
+#include <algorithm>
+#include <numeric>
 #include <cassert>
 #include <iostream>
 
@@ -34,7 +18,6 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #include "gtstall_groups.h"
 #include "gtstall_parameters.h"
 #include "gtstall_serverstates.h"
-#include "gtstdebugdialog.h"
 #include "gtstgroups.h"
 #include "ipaddress.h"
 #include "gtstlogfile.h"
